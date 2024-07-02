da35502efb42b9e5758e9b203b97dabc2c96087d
Rup Chowdhury <rupbd1@gmail.com>
2024-07-02 22:58:11 +0600

    Update main.dart

--- first/lib/main.dart
+++ first/lib/main.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(IntroApp());
 }
 
 class IntroApp extends StatelessWidget{
 
+  @override
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          title: Text("Home of Ruplaal"),
           backgroundColor: Colors.blueAccent,
         ),
         body: Center(
-          child: Text("Hello World"),
+          child: Text("Hello Ostad Rup"),
         ),
       ),
-    )
+    );
   }
 
 }
