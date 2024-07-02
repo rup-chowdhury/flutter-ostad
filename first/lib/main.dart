6cd2021495c0a2c32a8f2cdac180b2902b93ceb7
Rup Chowdhury <rupbd1@gmail.com>
2024-07-02 22:51:59 +0600

    Update main.dart

--- first/lib/main.dart
+++ first/lib/main.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text('Hello World'),
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+      child: Text("Hello World"),
+    ),
     ),
   ));
 }
