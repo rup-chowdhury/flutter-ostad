e79e79f17ca5e1f63b84632e0840bb4f229e2d68
Rup Chowdhury <rupbd1@gmail.com>
2024-07-02 22:54:47 +0600

    Update main.dart

--- first/lib/main.dart
+++ first/lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
+  runApp();
+}
+
+class IntroApp extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Center(
+          child: Text("Hello World"),
+        ),
       ),
-      body: Center(
-      child: Text("Hello World"),
-    ),
-    ),
-  ));
+    )
+  }
+
 }
 
 
