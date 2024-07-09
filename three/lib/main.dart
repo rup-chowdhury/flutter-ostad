c3afd25a44748476c2c7a38f92fe2125aad532ec
Rup Chowdhury <rupbd1@gmail.com>
2024-07-09 22:36:15 +0600

    add basic codes

--- three/lib/main.dart
+++ three/lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(homeApp());
+}
+
+class homeApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.blueGrey,
+              title: Text("Home Page"),
+            ),
+            body: Center(
+              child: Text("Hello World"),
+            )
+        )
+    );
+  }
 }
