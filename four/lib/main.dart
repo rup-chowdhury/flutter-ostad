8df943cb7065953c240059cfe7f51ba30a439f0e
Rup Chowdhury <rupbd1@gmail.com>
2024-07-17 14:11:10 +0600

    Update main.dart

--- four/lib/main.dart
+++ four/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// 
+
 void main() {
   runApp(MyApp());
 }
@@ -11,7 +13,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Home(),
+      title: 'Intro App',
     );
   }
 }
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
 
