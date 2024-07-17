370327d7acd257425a7b7dff8ead7ec85cb40185
Rup Chowdhury <rupbd1@gmail.com>
2024-07-17 21:21:39 +0600

    making home a stateful widget

--- four/lib/main.dart
+++ four/lib/main.dart
@@ -19,30 +19,35 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Home'),
-        backgroundColor: Colors.blueAccent,
-      ),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Home'),
+          backgroundColor: Colors.blueAccent,
+        ),
 
-      body: ListView.builder(
-        itemCount: 1000,
-        itemBuilder: (context, index) {
-          return SizedBox(
-            width: 100,
-            height: 100,
-            child: Center(
-                child: Text((index+1).toString())),
-          );
-        },
-      )
-    );
+        body: ListView.builder(
+          itemCount: 1000,
+          itemBuilder: (context, index) {
+            return SizedBox(
+              width: 100,
+              height: 100,
+              child: Center(
+                  child: Text((index+1).toString())),
+            );
+          },
+        )
+    );;
   }
 }
 
