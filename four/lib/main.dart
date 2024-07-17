b3161a0e28682153df0f0eb3c0c44bceebef57b4
Rup Chowdhury <rupbd1@gmail.com>
2024-07-17 14:12:54 +0600

    Update main.dart

--- four/lib/main.dart
+++ four/lib/main.dart
@@ -24,7 +24,12 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+    );
   }
 }
 
