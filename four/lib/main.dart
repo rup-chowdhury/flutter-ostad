ba3da8dfe16000c8bc266bae822ed533175abbd5
Rup Chowdhury <rupbd1@gmail.com>
2024-07-18 15:51:47 +0600

    Update main.dart

--- four/lib/main.dart
+++ four/lib/main.dart
@@ -44,6 +44,7 @@ class _HomeState extends State<Home> {
             }, separatorBuilder: (BuildContext context, int index) {
               return Divider(
                 height: 30,
+                color: Colors.blue,
               );
         },),
 
