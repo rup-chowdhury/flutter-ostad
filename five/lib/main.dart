f901ecc7e43c20bb61bd7253ab45e82e356e8388
Rup Chowdhury <rupbd1@gmail.com>
2024-07-26 20:59:21 +0600

    add color and wrap with center

--- five/lib/main.dart
+++ five/lib/main.dart
@@ -35,9 +35,12 @@ class _HomeState extends State<Home> {
         title: Text("Day after fredom"),
         backgroundColor: Colors.blueAccent,
       ),
-      body: Container(
-        height: 100,
-        width: 100,
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          color: Colors.greenAccent,
+        ),
       ),
     );
   }
