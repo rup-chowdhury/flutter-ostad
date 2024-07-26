1eab81be5ab195ef72c1af0595f4b72df57ed12e
Rup Chowdhury <rupbd1@gmail.com>
2024-07-26 20:43:25 +0600

    adding another stateless into one

---
+++ five/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(IntroApp());
+}
+
+class IntroApp extends StatelessWidget {
+  const IntroApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      title: "Intro App",
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
