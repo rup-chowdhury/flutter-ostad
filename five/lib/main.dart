af2b3afb10b1ecfb67e4f9c16ef721ba2418d83f
Rup Chowdhury <rupbd1@gmail.com>
2024-07-27 17:09:23 +0600

    background color of dialog bar

--- five/lib/main.dart
+++ five/lib/main.dart
@@ -141,6 +141,7 @@ class _HomeState extends State<Home> {
                       // borderRadius: BorderRadius.zero,
                       borderRadius: BorderRadius.circular(8.0)
                     ),
+                    backgroundColor: Colors.white,
                   );
                 });
                 },
