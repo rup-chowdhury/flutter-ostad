ae3f151c294b9893c10c4a27892e90e17acad055
Rup Chowdhury <rupbd1@gmail.com>
2024-07-28 16:01:19 +0600

    stateless widget

--- six/lib/main.dart
+++ six/lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+//Module 9 Class 1 
+
 void main(){
-  
+  runApp(six());
+}
+
+class six extends StatelessWidget {
+  const six({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
 }
