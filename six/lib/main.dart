ab8b0b06453310462b98e3577832c7d330b9f937
Rup Chowdhury <rupbd1@gmail.com>
2024-07-28 16:38:04 +0600

    disabling the TextBox

--- six/lib/main.dart
+++ six/lib/main.dart
@@ -35,6 +35,7 @@ class Home extends StatelessWidget {
         child: Column(
           children: [
             TextField(
+              enabled: false,
               decoration: InputDecoration(
                 hintText: "Enter your name",
                 hintStyle: TextStyle(
