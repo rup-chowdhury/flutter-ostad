f1bec067a304e0d34069308595606b63e38f48a8
Rup Chowdhury <rupbd1@gmail.com>
2024-07-28 17:37:21 +0600

    Description Box

--- six/lib/main.dart
+++ six/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 // Textfield
 
-void main(){
+void main() {
   runApp(six());
 }
 
@@ -38,38 +38,56 @@ class Home extends StatelessWidget {
               enabled: true,
               decoration: InputDecoration(
                 label: Text("Name"),
-                labelStyle: TextStyle(
-                  letterSpacing: 2
+                labelStyle: TextStyle(letterSpacing: 2),
+                icon: Icon(
+                  Icons.account_circle,
+                  size: 45,
+                  color: Colors.blueGrey[800],
                 ),
-                icon: Icon(Icons.account_circle, size: 45, color: Colors.blueGrey[800],),
                 prefixIcon: Icon(Icons.arrow_drop_down_circle_outlined),
                 suffixIcon: TextButton(onPressed: () {}, child: Text("Search")),
                 hintText: "Enter your name",
-                hintStyle: TextStyle(
-                  color: Colors.grey.withOpacity(0.7)
-                ),
-                border: OutlineInputBorder(),
+                hintStyle: TextStyle(color: Colors.grey.withOpacity(0.7)),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide(color: Colors.red, width: 2)),
                 disabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: BorderSide(
-                    color: Colors.red,
-                    width: 2
-                  )
-                ),
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide(color: Colors.red, width: 2)),
                 focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide(
-                        color: Colors.blue,
-                        width: 2
-                    )
-                ),
+                    borderSide: BorderSide(color: Colors.blue, width: 2)),
+              ),
+            ),
+            SizedBox(height: 20,),
+            TextField(
+              enabled: true,
+              maxLines: 5,
+              maxLength: 200,
+              onTap: () {
+                print("Tapped on Description");
+              },
+              onChanged: (String value) {
+                print(value);
+              },
+              decoration: InputDecoration(
+                label: Text("Description"),
+                labelStyle: TextStyle(letterSpacing: 2, fontSize: 16),
+                alignLabelWithHint: true,
+                hintText: "Enter your description",
+                hintStyle: TextStyle(color: Colors.grey.withOpacity(0.7)),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide(color: Colors.red, width: 2)),
+                disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide(color: Colors.red, width: 2)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide(color: Colors.blue, width: 2)),
               ),
             )
           ],
-
         ),
       ),
     );
   }
 }
-
