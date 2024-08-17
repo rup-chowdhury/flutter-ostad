5ab6c52b8e2385337a80aee255624ec97254bc22
Rup Chowdhury <rupbd1@gmail.com>
2024-08-17 14:47:12 +0600

    CupertinoPageScaffold instead Scaffold

--- seven/lib/profile_page.dart
+++ seven/lib/profile_page.dart
@@ -5,6 +5,7 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CupertinoPageScaffold(
+        child:);
   }
 }
