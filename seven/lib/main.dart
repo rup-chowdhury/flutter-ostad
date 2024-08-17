064b3425ac81c017fe7b5754a2ac1c489daef225
Rup Chowdhury <rupbd1@gmail.com>
2024-08-17 14:32:34 +0600

    separating the settings screen

--- seven/lib/main.dart
+++ seven/lib/main.dart
@@ -144,17 +144,3 @@ class IntroApp extends StatelessWidget {
 }
 
 
-
-class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-    );
-  }
-}
