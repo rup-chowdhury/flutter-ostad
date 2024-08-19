b76849ce5c0f2f27026242661a20783fd783fd9c
Rup Chowdhury <rupbd1@gmail.com>
2024-08-19 23:52:12 +0600

    Update main.dart

--- eight/lib/main.dart
+++ eight/lib/main.dart
@@ -48,12 +48,11 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
 
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
