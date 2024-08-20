76ed30e11ab1a0a446223745de701a4f62040cb7
Rup Chowdhury <rupbd1@gmail.com>
2024-08-20 23:45:52 +0600

    Update main.dart

--- eight/lib/main.dart
+++ eight/lib/main.dart
@@ -46,8 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
       ),
     );
   }
