a5130590ee9fce3e5bf64020f8fd4825b68efedc
Rup Chowdhury <rupbd1@gmail.com>
2024-08-20 23:45:43 +0600

    Update main.dart

--- eight/lib/main.dart
+++ eight/lib/main.dart
@@ -48,8 +48,6 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
 
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
       ),
     );
   }
