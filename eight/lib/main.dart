b370871a039636092e0d0024485890d8314e3b5e
Rup Chowdhury <rupbd1@gmail.com>
2024-08-21 17:25:58 +0600

    Update main.dart

--- eight/lib/main.dart
+++ eight/lib/main.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(app());
+  runApp(MyApp());
 }
 
-class app extends StatelessWidget {
-  const app({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: Home(),
+    );
   }
 }
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+    );
+  }
+}
+
