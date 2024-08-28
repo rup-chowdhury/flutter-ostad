12a87badbb983167bfab531a0818fd2a52e4b1af
Rup Chowdhury <rupbd1@gmail.com>
2024-08-28 22:09:30 +0600

    import device preview

--- eight/lib/main.dart
+++ eight/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
 // Class name: MediaQuery | Wrap | LayoutBuilder | WidgetBuilder  ( 15 August, 2024)
 
