51aeba8cb706eeac67989afce864f77ce468daad
Rup Chowdhury <rupbd1@gmail.com>
2024-08-28 22:46:52 +0600

    Update main.dart

--- eight/lib/main.dart
+++ eight/lib/main.dart
@@ -16,7 +16,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       home: Home(),
     );
   }
