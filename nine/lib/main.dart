2c2c711f76ca0a02b8d97453afd09479dd83f1a4
Rup Chowdhury <rupbd1@gmail.com>
2024-08-29 16:08:06 +0600

    .

--- nine/lib/main.dart
+++ nine/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:device_preview/device_preview.dart';
 // Topic Name : Aspect Ratio | Expanded | FractionallySizedBox
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) => MyApp(), // Wrap your app
+  ),);
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +14,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+      home: HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatelessWidget {
+  const HomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
