7d04c26c37442e3b590176b838cb46d4ea087a7f
Rup Chowdhury <rupbd1@gmail.com>
2024-08-30 13:27:46 +0600

    Update main.dart

--- nine/lib/main.dart
+++ nine/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         useInheritedMediaQuery: true,
         locale: DevicePreview.locale(context),
         builder: DevicePreview.appBuilder,
