e1d2122d9803608984da4815708417b9d5358a3c
Rup Chowdhury <rupbd1@gmail.com>
2024-09-11 16:22:44 +0600

    Update calculator_app.dart

---
+++ project_calculator_app/lib/calculator_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Calculator"),
+      ),
+    );
+  }
+}
