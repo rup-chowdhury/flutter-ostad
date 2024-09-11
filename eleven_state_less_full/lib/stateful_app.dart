a6d3b77364822809a771a505413cbcde09c8fe58
Rup Chowdhury <rupbd1@gmail.com>
2024-09-11 22:59:47 +0600

    stateful and stateless

---
+++ eleven_state_less_full/lib/stateful_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StatefulApp extends StatelessWidget {
+  const StatefulApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
