82ef7d82d89016f3cd813836f359e8e801f1da8e
Rup Chowdhury <rupbd1@gmail.com>
2024-09-11 23:03:54 +0600

    removing debug banner

--- eleven_state_less_full/lib/main.dart
+++ eleven_state_less_full/lib/main.dart
@@ -11,6 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
       home: StatefulApp(),
     );
   }
