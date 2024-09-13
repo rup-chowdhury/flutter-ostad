e05f0cb1971aeb3c17cb5444386c528fa6b171eb
Rup Chowdhury <rupbd1@gmail.com>
2024-09-13 13:36:19 +0600

    Create calculator_app.dart

---
+++ project_calculator_app/lib/calculator_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: ,
+    );
+  }
+}
