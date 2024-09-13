ee1cf0136b962be8b5c07558eb5a42fc6785b9db
Rup Chowdhury <rupbd1@gmail.com>
2024-09-13 18:53:04 +0600

    Result show

--- project_calculator_app/lib/home_screen.dart
+++ project_calculator_app/lib/home_screen.dart
@@ -8,9 +8,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   final TextEditingController _firstNumTEController = TextEditingController();
   final TextEditingController _secondNumTEController = TextEditingController();
+
+  double _result = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +27,12 @@ class _HomeScreenState extends State<HomeScreen> {
               controller: _firstNumTEController,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
-                hintText: "First Number",
-                hintStyle: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.transparent.withOpacity(0.7)
-                ),
-                labelText: "First Number"
-              ),
+                  hintText: "First Number",
+                  hintStyle: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.transparent.withOpacity(0.7)),
+                  labelText: "First Number"),
             ),
             SizedBox(height: 16),
             TextField(
@@ -44,45 +43,45 @@ class _HomeScreenState extends State<HomeScreen> {
                   hintStyle: TextStyle(
                       fontSize: 16,
                       fontWeight: FontWeight.w400,
-                      color: Colors.transparent.withOpacity(0.7)
-                  ),
-                  labelText: "Second Number"
-              ),
+                      color: Colors.transparent.withOpacity(0.7)),
+                  labelText: "Second Number"),
             ),
             SizedBox(height: 32),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                IconButton(onPressed: (){
-
-                }, icon: Icon(Icons.add)),
-                IconButton(onPressed: (){
-
-                }, icon: Icon(Icons.remove)),
-                TextButton(onPressed: (){
-
-                }, child: Text("x",
-                  style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black.withOpacity(0.8),
-                  fontSize: 20
-                ),)),
-                TextButton(onPressed: (){
-
-                }, child: Text("/",
+                IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.remove)),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "x",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black.withOpacity(0.8),
+                          fontSize: 20),
+                    )),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "/",
                     style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black.withOpacity(0.8),
-                    fontSize: 18
-                ),),),
-
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black.withOpacity(0.8),
+                        fontSize: 18),
+                  ),
+                ),
               ],
-            )
-
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Text(" Result : $_result", style: TextStyle(
+              fontSize: 18
+            ),)
           ],
         ),
       ),
     );
   }
-
 }
