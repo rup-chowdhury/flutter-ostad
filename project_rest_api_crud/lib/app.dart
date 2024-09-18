cf92c7105ff5a176bad5a09a195cea1eeea348b1
Rup Chowdhury <rupbd1@gmail.com>
2024-09-18 22:29:46 +0600

    Update app.dart

---
+++ project_rest_api_crud/lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CrudApp extends StatelessWidget {
+  const CrudApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+    );
+  }
+}
