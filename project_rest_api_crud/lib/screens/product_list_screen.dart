976c6a38e7a826111cd040f3596a2ffc956f587c
Rup Chowdhury <rupbd1@gmail.com>
2024-09-19 23:05:04 +0600

    Update product_list_screen.dart

--- project_rest_api_crud/lib/screens/product_list_screen.dart
+++ project_rest_api_crud/lib/screens/product_list_screen.dart
@@ -16,37 +16,40 @@ class _ProductListScreenState extends State<ProductListScreen> {
       ),
       body: Column(
         children: [
-          ListTile(
-            tileColor: Colors.white,
-            title: const Text("Product Name"),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("Product Code: code"),
-                const Text("Price: \$120"),
-                const Text("Quantity: 2"),
-                const Text("Total Price: \$240"),
-                const Divider(),
-                ButtonBar(
-                  children: [
-                    TextButton.icon(
-                      onPressed: () {},
-                      label: const Text("Edit", style: TextStyle(
-                        color: Colors.blue
-                      ),),
-                      icon: Icon(Icons.edit, color: Colors.blue,),
-                    ),
-                    TextButton.icon(
-                      onPressed: () {},
-                      label: Text("Delete", style: TextStyle(
-                        color: Colors.red
-                      ),),
-                      icon: Icon(Icons.delete, color: Colors.red,),
-                    )
-                  ],
-                )
-              ],
+          Padding(
+            padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+            child: ListTile(
+              tileColor: Colors.white,
+              title: const Text("Product Name"),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Product Code: code"),
+                  const Text("Price: \$120"),
+                  const Text("Quantity: 2"),
+                  const Text("Total Price: \$240"),
+                  const Divider(),
+                  ButtonBar(
+                    children: [
+                      TextButton.icon(
+                        onPressed: () {},
+                        label: const Text("Edit", style: TextStyle(
+                          color: Colors.blue
+                        ),),
+                        icon: Icon(Icons.edit, color: Colors.blue,),
+                      ),
+                      TextButton.icon(
+                        onPressed: () {},
+                        label: Text("Delete", style: TextStyle(
+                          color: Colors.red
+                        ),),
+                        icon: Icon(Icons.delete, color: Colors.red,),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           )
         ],
