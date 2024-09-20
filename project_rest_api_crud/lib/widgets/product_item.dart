d86d8a9fd4eb88413dd90e5d06a40854b489f8f2
Rup Chowdhury <rupbd1@gmail.com>
2024-09-20 20:53:41 +0600

    taking the ProductItem class into a different file

---
+++ project_rest_api_crud/lib/widgets/product_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      tileColor: Colors.white,
+      title: const Text("Product Name"),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("Product Code: code"),
+          const Text("Price: \$120"),
+          const Text("Quantity: 2"),
+          const Text("Total Price: \$240"),
+          const Divider(),
+          ButtonBar(
+            children: [
+              TextButton.icon(
+                onPressed: () {},
+                label: const Text("Edit", style: TextStyle(
+                    color: Colors.blue
+                ),),
+                icon: const Icon(Icons.edit, color: Colors.blue,),
+              ),
+              TextButton.icon(
+                onPressed: () {},
+                label: const Text("Delete", style: TextStyle(
+                    color: Colors.red
+                ),),
+                icon: const Icon(Icons.delete, color: Colors.red,),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
