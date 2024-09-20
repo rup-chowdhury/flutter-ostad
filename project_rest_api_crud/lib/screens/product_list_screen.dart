dfbdcdda8ab1535104bcbfe30604be3111bb83f5
Rup Chowdhury <rupbd1@gmail.com>
2024-09-20 20:42:52 +0600

    extracting the listTile into widget

--- project_rest_api_crud/lib/screens/product_list_screen.dart
+++ project_rest_api_crud/lib/screens/product_list_screen.dart
@@ -18,39 +18,50 @@ class _ProductListScreenState extends State<ProductListScreen> {
         children: [
           Padding(
             padding: const EdgeInsets.only(left: 12.0, right: 12.0),
-            child: ListTile(
-              tileColor: Colors.white,
-              title: const Text("Product Name"),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Product Code: code"),
-                  const Text("Price: \$120"),
-                  const Text("Quantity: 2"),
-                  const Text("Total Price: \$240"),
-                  const Divider(),
-                  ButtonBar(
-                    children: [
-                      TextButton.icon(
-                        onPressed: () {},
-                        label: const Text("Edit", style: TextStyle(
-                          color: Colors.blue
-                        ),),
-                        icon: Icon(Icons.edit, color: Colors.blue,),
-                      ),
-                      TextButton.icon(
-                        onPressed: () {},
-                        label: Text("Delete", style: TextStyle(
-                          color: Colors.red
-                        ),),
-                        icon: Icon(Icons.delete, color: Colors.red,),
-                      )
-                    ],
-                  )
-                ],
+            child: ProductItem(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      tileColor: Colors.white,
+      title: const Text("Product Name"),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("Product Code: code"),
+          const Text("Price: \$120"),
+          const Text("Quantity: 2"),
+          const Text("Total Price: \$240"),
+          const Divider(),
+          ButtonBar(
+            children: [
+              TextButton.icon(
+                onPressed: () {},
+                label: const Text("Edit", style: TextStyle(
+                  color: Colors.blue
+                ),),
+                icon: Icon(Icons.edit, color: Colors.blue,),
               ),
-            ),
+              TextButton.icon(
+                onPressed: () {},
+                label: Text("Delete", style: TextStyle(
+                  color: Colors.red
+                ),),
+                icon: Icon(Icons.delete, color: Colors.red,),
+              )
+            ],
           )
         ],
       ),
