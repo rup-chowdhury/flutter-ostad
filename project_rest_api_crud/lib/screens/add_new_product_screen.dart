5e412948f6f382960dbee9597489b579d9d4b0f9
Rup Chowdhury <rupbd1@gmail.com>
2024-09-20 21:37:37 +0600

    add product screen design

--- project_rest_api_crud/lib/screens/add_new_product_screen.dart
+++ project_rest_api_crud/lib/screens/add_new_product_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class AddNewProductScreen extends StatefulWidget {
   const AddNewProductScreen({super.key});
@@ -8,8 +11,71 @@ class AddNewProductScreen extends StatefulWidget {
 }
 
 class _AddNewProductScreenState extends State<AddNewProductScreen> {
+  final TextEditingController _productNameTEController =
+      TextEditingController();
+  final TextEditingController _unitPriceTEController = TextEditingController();
+  final TextEditingController _quantityTEController = TextEditingController();
+  final TextEditingController _totalPriceTEController = TextEditingController();
+  final TextEditingController _productImageTEController =
+      TextEditingController();
+  final TextEditingController _productCodeTEController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add New Product"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _productNameTEController,
+                  decoration: InputDecoration(
+                      hintText: "Name", labelText: "Product Name"),
+                ),
+                TextFormField(
+                  controller: _unitPriceTEController,
+                  decoration:
+                      InputDecoration(hintText: "Price", labelText: "Unit Price"),
+                ),
+                TextFormField(
+                  controller: _quantityTEController,
+                  decoration: InputDecoration(
+                      hintText: "Quantity", labelText: "Quantity"),
+                ),
+                TextFormField(
+                  controller: _totalPriceTEController,
+                  decoration: InputDecoration(
+                      hintText: "Total Price", labelText: "Total Price"),
+                ),
+                TextFormField(
+                  controller: _productImageTEController,
+                  decoration: InputDecoration(
+                      hintText: "Image", labelText: "Product Image"),
+                ),
+                TextFormField(
+                  controller: _productCodeTEController,
+                  decoration: InputDecoration(
+                      hintText: "Code", labelText: "Product Code"),
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                CupertinoButton(child: Text("Add"), onPressed: (){
+          
+                },
+                color: Colors.blue,
+                padding: EdgeInsets.symmetric(horizontal: 140),)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
