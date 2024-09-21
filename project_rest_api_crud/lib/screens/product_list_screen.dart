84b04595ea963bd5740b42ec6fcb4b0fb74ece15
Rup Chowdhury <rupbd1@gmail.com>
2024-09-21 12:52:54 +0600

    Read data from API complete

--- project_rest_api_crud/lib/screens/product_list_screen.dart
+++ project_rest_api_crud/lib/screens/product_list_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:project_rest_api_crud/models/product.dart';
@@ -13,7 +15,6 @@ class ProductListScreen extends StatefulWidget {
 
 class _ProductListScreenState extends State<ProductListScreen> {
   List<Product> productList = [];
-
 
   @override
   void initState() {
@@ -31,22 +32,28 @@ class _ProductListScreenState extends State<ProductListScreen> {
         backgroundColor: Colors.blueGrey[100],
       ),
       body: ListView.builder(
-          itemCount: 20,
-          itemBuilder: (context, index){
-            return const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: ProductItem(),
+          itemCount: productList.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ProductItem(
+                product: productList[index],
+              ),
             );
           }),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context){
-          return const AddNewProductScreen();
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return const AddNewProductScreen();
+              },
+            ),
+          );
         },
-        ),
-        );
-      },
         backgroundColor: Colors.blueGrey[200],
-      child: const Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
@@ -57,13 +64,27 @@ class _ProductListScreenState extends State<ProductListScreen> {
     //API calling part
 
     Uri uri = Uri.parse("http://164.68.107.70:6060/api/v1/ReadProduct");
-    Response response = await get(uri);                          // receiving the response from the API
-
+    Response response = await get(uri); // receiving the response from the API
 
     print(response);
     print(response.statusCode);
     print(response.body);
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+      for (var item in jsonResponse['data']) {
+        Product product = Product(
+            id: item['_id'],
+            productName: item['ProductName'],
+            productCode: item['ProductCode'],
+            productImage: item['Img'],
+            unitPrice: item['UnitPrice'],
+            quantity: item['Qty'],
+            totalPrice: item['TotalPrice'],
+            createdAt: item['CreatedDate']);
+        productList.add(product);
+      }
+    }
+    setState(() {});
   }
 }
-
-
--- project_rest_api_crud/lib/widgets/product_item.dart
+++ project_rest_api_crud/lib/widgets/product_item.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:project_rest_api_crud/models/product.dart';
 import 'package:project_rest_api_crud/screens/update_product_screen.dart';
 
 class ProductItem extends StatelessWidget {
   const ProductItem({
-    super.key,
+    super.key, required this.product,
   });
+
+  final Product product;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       tileColor: Colors.white,
-      title: const Text("Product Name"),
+      title: Text(product.productName),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text("Product Code: code"),
-          const Text("Price: \$120"),
-          const Text("Quantity: 2"),
-          const Text("Total Price: \$240"),
+          Text("Product Code: ${product.productCode}"),
+          Text("Price: ${product.unitPrice}"),
+          Text("Quantity: ${product.quantity}"),
+          Text("Total Price: ${product.totalPrice}"),
           const Divider(),
           ButtonBar(
             children: [
