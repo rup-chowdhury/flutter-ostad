2d9eaf18922595ee5c831b02e12957633ec64547
Rup Chowdhury <rupbd1@gmail.com>
2024-09-22 16:28:08 +0600

    Create app.dart

---
+++ project_sample_bank_management/lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:project_sample_bank_management/Screens/home_screen.dart';
+
+class BankManagementApp extends StatelessWidget {
+  const BankManagementApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreen();
+  }
+}
