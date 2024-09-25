ffd0ec84804a7f9846874eac5dfc99fe646cb2a2
Rup Chowdhury <rupbd1@gmail.com>
2024-09-25 23:07:15 +0600

    Update app.dart

--- project_to_do_app/lib/app.dart
+++ project_to_do_app/lib/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:project_to_do_app/ui/screens/splash_screen.dart';
 
 class TaskManagerApp extends StatelessWidget {
   const TaskManagerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: SplashScreen(),
+    );
   }
 }
