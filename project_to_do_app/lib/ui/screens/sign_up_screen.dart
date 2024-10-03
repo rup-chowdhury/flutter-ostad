95a503e4e7c2e91934cd89461c62b05ea8796700
Rup Chowdhury <rupbd1@gmail.com>
2024-10-03 23:51:58 +0600

    Update sign_up_screen.dart

--- project_to_do_app/lib/ui/screens/sign_up_screen.dart
+++ project_to_do_app/lib/ui/screens/sign_up_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:project_to_do_app/ui/screens/sign_up_screen.dart';
 import 'package:project_to_do_app/ui/utils/app_colors.dart';
 import 'package:project_to_do_app/ui/widgets/screen_background.dart';
 
