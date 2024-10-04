7e3dfcc2bf16b38a1611db334a917f909067b80a
Rup Chowdhury <rupbd1@gmail.com>
2024-10-04 12:04:46 +0600

    Update forgot_password_email_screen.dart

--- project_to_do_app/lib/ui/screens/forgot_password_email_screen.dart
+++ project_to_do_app/lib/ui/screens/forgot_password_email_screen.dart
@@ -1,15 +1,16 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:project_to_do_app/ui/utils/app_colors.dart';
 import 'package:project_to_do_app/ui/widgets/screen_background.dart';
 
-class ForgotPasswordEmailScreen extends StatefulWidget {
-  const ForgotPasswordEmailScreen({super.key});
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
 
   @override
-  State<ForgotPasswordEmailScreen> createState() => _ForgotPasswordEmailScreenState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _ForgotPasswordEmailScreenState extends State<ForgotPasswordEmailScreen> {
+class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
@@ -22,29 +23,108 @@ class _ForgotPasswordEmailScreenState extends State<ForgotPasswordEmailScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(height: 82,),
+                const SizedBox(
+                  height: 82,
+                ),
                 Text(
-                  "Your Email Address",
-                  style:
-                      textTheme.displaySmall?.copyWith(fontWeight: FontWeight.w600),
+                  "Join With Us",
+                  style: textTheme.displaySmall
+                      ?.copyWith(fontWeight: FontWeight.w600),
                 ),
-                const SizedBox(height: 24,),
-                TextFormField(decoration: InputDecoration(
-                  hintText: "Email"
-                ),),
-                SizedBox(height: 8,),
-                TextFormField(decoration: InputDecoration(
-                  hintText: "Password"
-                ),),
-                SizedBox(height: 24,),
-                ElevatedButton(onPressed: (){
-                }, child: Icon(Icons.arrow_circle_right_outlined),
+                const SizedBox(
+                  height: 24,
                 ),
+                _buildSignUpForm(),
+                const SizedBox(
+                  height: 24,
+                ),
+                _buildHaveAccountSection(),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildSignUpForm() {
+    return Column(
+      children: [
+        TextFormField(
+          decoration: const InputDecoration(hintText: "Email"),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        TextFormField(
+          decoration: const InputDecoration(hintText: "First Name"),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        TextFormField(
+          decoration: const InputDecoration(hintText: "Last Name"),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        TextFormField(
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(hintText: "Mobile"),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        TextFormField(
+          decoration: const InputDecoration(hintText: "Password"),
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        ElevatedButton(
+          onPressed: _onTapNextButton,
+          child: const Icon(Icons.arrow_circle_right_outlined),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildHaveAccountSection() {
+    return Center(
+      child: Column(
+        children: [
+          RichText(
+            text: TextSpan(
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14,
+                    letterSpacing: 0.5),
+                text: "Have account? ",
+                children: [
+                  TextSpan(
+                      text: "Sign In",
+                      style: TextStyle(
+                        color: AppColor.themeColor,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = _onTapSignIn),
+                ]),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onTapNextButton() {
+    //TODO: implement action for Next Button
+  }
+
+  void _onTapForgotPassword() {
+    //TODO: implement forgot password button action
+  }
+
+  void _onTapSignIn() {
+    //TODO: action for pressing Sign up button
+    Navigator.pop(context);
   }
 }
