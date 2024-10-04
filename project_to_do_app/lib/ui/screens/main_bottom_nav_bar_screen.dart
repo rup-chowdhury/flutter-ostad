f309111761f318214411a35bb809cf87320e6cbf
Rup Chowdhury <rupbd1@gmail.com>
2024-10-04 23:50:10 +0600

    .

--- project_to_do_app/lib/ui/screens/main_bottom_nav_bar_screen.dart
+++ project_to_do_app/lib/ui/screens/main_bottom_nav_bar_screen.dart
@@ -3,6 +3,8 @@ import 'package:project_to_do_app/ui/screens/cancelled_task_screen.dart';
 import 'package:project_to_do_app/ui/screens/completed_task_screen.dart';
 import 'package:project_to_do_app/ui/screens/new_task_screen.dart';
 import 'package:project_to_do_app/ui/screens/progress_task_screen.dart';
+import 'package:project_to_do_app/ui/utils/app_colors.dart';
+import 'package:project_to_do_app/ui/utils/assets_path.dart';
 
 class MainBottomNavBarScreen extends StatefulWidget {
   const MainBottomNavBarScreen({super.key});
@@ -12,7 +14,6 @@ class MainBottomNavBarScreen extends StatefulWidget {
 }
 
 class _MainBottomNavBarScreenState extends State<MainBottomNavBarScreen> {
-
   int _selectedIndex = 0;
 
   final List<Widget> _screens = const [
@@ -33,23 +34,23 @@ class _MainBottomNavBarScreenState extends State<MainBottomNavBarScreen> {
             setState(() {});
           },
           destinations: const [
-        NavigationDestination(
-          icon: Icon(Icons.new_label),
-          label: "New",
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.check_box),
-          label: "Completed",
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.close),
-          label: "Cancelled",
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.access_alarm_rounded),
-          label: "In Progress",
-        ),
-      ]),
+            NavigationDestination(
+              icon: Icon(Icons.new_label),
+              label: "New",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.check_box),
+              label: "Completed",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.close),
+              label: "Cancelled",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.access_alarm_rounded),
+              label: "In Progress",
+            ),
+          ]),
     );
   }
 }
