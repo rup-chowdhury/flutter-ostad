073fc5df90be69d533cc021fb1becd410fd36011
Rup Chowdhury <rupbd1@gmail.com>
2024-10-04 18:26:14 +0600

    bottom navigation bar setup

--- project_to_do_app/lib/ui/screens/main_bottom_nav_bar_screen.dart
+++ project_to_do_app/lib/ui/screens/main_bottom_nav_bar_screen.dart
@@ -11,7 +11,24 @@ class _MainBottomNavBarScreenState extends State<MainBottomNavBarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      bottomNavigationBar: NavigationBar(destinations: const [
+        NavigationDestination(
+          icon: Icon(Icons.new_label),
+          label: "New",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.check_box),
+          label: "Completed",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.close),
+          label: "Cancelled",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.access_alarm_rounded),
+          label: "In Progress",
+        ),
+      ]),
     );
   }
 }
