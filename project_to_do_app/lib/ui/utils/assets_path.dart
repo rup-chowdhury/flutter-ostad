0362f683e42a34126ccbc7c2bd9089f0bb8f6fef
Rup Chowdhury <rupbd1@gmail.com>
2024-10-04 23:49:37 +0600

    Update assets_path.dart

--- project_to_do_app/lib/ui/utils/assets_path.dart
+++ project_to_do_app/lib/ui/utils/assets_path.dart
@@ -3,4 +3,5 @@ class AssetsPath {
 
   static const String logoSvg = "$_imagePath/logo.svg";
   static const String backgroundSvg = "$_imagePath/background.svg";
+  static const String profilePicJpg = "$_imagePath/rup_image.jpg";
 }
