becb48e72c72e62d5746f40eb75601597d4abd8a
Rup Chowdhury <rupbd1@gmail.com>
2024-10-04 17:59:01 +0600

    Create main_bottom_nav_bar_screen.dart

---
+++ project_to_do_app/lib/ui/screens/main_bottom_nav_bar_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainBottomNavBarScreen extends StatefulWidget {
+  const MainBottomNavBarScreen({super.key});
+
+  @override
+  State<MainBottomNavBarScreen> createState() => _MainBottomNavBarScreenState();
+}
+
+class _MainBottomNavBarScreenState extends State<MainBottomNavBarScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
