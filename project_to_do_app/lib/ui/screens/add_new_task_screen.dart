225779021e53c11479a3cd20039ce5f14d14f6a4
Rup Chowdhury <rupbd1@gmail.com>
2024-10-06 16:15:12 +0600

    add new task screen UI complete

--- project_to_do_app/lib/ui/screens/add_new_task_screen.dart
+++ project_to_do_app/lib/ui/screens/add_new_task_screen.dart
@@ -11,8 +11,35 @@ class AddNewTaskScreen extends StatefulWidget {
 class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: TMAppBar(),
+    return Scaffold(
+      appBar: const TMAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 48,),
+              Text("Add New Task", style: Theme.of(context).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.w700),),
+              const SizedBox(height: 24,),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Title"
+                ),
+              ),
+              const SizedBox(height: 8,),
+              TextFormField(
+                maxLines: 5,
+                decoration: const InputDecoration(
+                    hintText: "Description"
+                ),
+              ),
+              const SizedBox(height: 16,),
+              ElevatedButton(onPressed: (){}, child: const Icon(Icons.arrow_circle_right_outlined))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
