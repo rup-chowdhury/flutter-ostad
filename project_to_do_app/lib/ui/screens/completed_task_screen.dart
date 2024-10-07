8f4f51dfef416b61394b5e13fe9cd82fbd19325e
Rup Chowdhury <rupbd1@gmail.com>
2024-10-07 16:54:50 +0600

    Update completed_task_screen.dart

--- project_to_do_app/lib/ui/screens/completed_task_screen.dart
+++ project_to_do_app/lib/ui/screens/completed_task_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:project_to_do_app/ui/widgets/task_card.dart';
 
 class CompletedTaskScreen extends StatelessWidget {
   const CompletedTaskScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Completed"),
+    return ListView.separated(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return TaskCard();
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(height: 8,);
+      },
     );
   }
 }
