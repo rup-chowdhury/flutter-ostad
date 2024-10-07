9d0d6f12487b28fb603739fdc250f7dd6611b487
Rup Chowdhury <rupbd1@gmail.com>
2024-10-07 15:06:25 +0600

    talking into a different file

--- project_to_do_app/lib/ui/screens/new_task_screen.dart
+++ project_to_do_app/lib/ui/screens/new_task_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_to_do_app/ui/screens/add_new_task_screen.dart';
 import 'package:project_to_do_app/ui/utils/app_colors.dart';
+import 'package:project_to_do_app/ui/widgets/task_card.dart';
 import 'package:project_to_do_app/ui/widgets/task_summary_card.dart';
 
 class NewTaskScreen extends StatefulWidget {
@@ -66,53 +67,5 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
   }
 }
 
-class TaskCard extends StatelessWidget {
-  const TaskCard({
-    super.key,
-  });
 
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      color: Colors.white,
-      margin: const EdgeInsets.symmetric(horizontal: 16),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Title of the Task", style: Theme.of(context).textTheme.titleSmall,),
-            const Text("Description of task",),
-            const Text("Date: 20/10/2024",),
-            const SizedBox(height: 8,),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Chip(
-                  label: const Text(
-                    "New", style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold
-                ),), shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24),
-                  side: const BorderSide(
-                    color: AppColor.themeColor
-                  )
-                ),),
-                Wrap(
-                  children: [
-                    IconButton(onPressed: (){}, icon: const Icon(Icons.edit)),
-                    IconButton(onPressed: (){}, icon: const Icon(Icons.delete)),
-                  ],
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
 
---
+++ project_to_do_app/lib/ui/widgets/task_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:project_to_do_app/ui/utils/app_colors.dart';
+
+class TaskCard extends StatelessWidget {
+  const TaskCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      color: Colors.white,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Title of the Task", style: Theme.of(context).textTheme.titleSmall,),
+            const Text("Description of task",),
+            const Text("Date: 20/10/2024",),
+            const SizedBox(height: 8,),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Chip(
+                  label: const Text(
+                    "New", style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold
+                  ),), shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                    side: const BorderSide(
+                        color: AppColor.themeColor
+                    )
+                ),),
+                Wrap(
+                  children: [
+                    IconButton(onPressed: (){}, icon: const Icon(Icons.edit)),
+                    IconButton(onPressed: (){}, icon: const Icon(Icons.delete)),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
