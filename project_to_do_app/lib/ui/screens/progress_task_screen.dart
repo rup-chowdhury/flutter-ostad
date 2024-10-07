4fe90136844c3641b7ea28bb2f29a0b570673360
Rup Chowdhury <rupbd1@gmail.com>
2024-10-07 16:54:57 +0600

    Update progress_task_screen.dart

--- project_to_do_app/lib/ui/screens/progress_task_screen.dart
+++ project_to_do_app/lib/ui/screens/progress_task_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:project_to_do_app/ui/widgets/task_card.dart';
 
 class ProgressTaskScreen extends StatelessWidget {
   const ProgressTaskScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Progress"),
+    return ListView.separated(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return TaskCard();
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(height: 8,);
+      },
     );
   }
 }
