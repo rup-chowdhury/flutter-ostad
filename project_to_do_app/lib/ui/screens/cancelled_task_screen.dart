ecd5e733780e0187d5eaeb987c8c55f114da834a
Rup Chowdhury <rupbd1@gmail.com>
2024-10-07 16:54:54 +0600

    Update cancelled_task_screen.dart

--- project_to_do_app/lib/ui/screens/cancelled_task_screen.dart
+++ project_to_do_app/lib/ui/screens/cancelled_task_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:project_to_do_app/ui/widgets/task_card.dart';
 
 class CancelledTaskScreen extends StatelessWidget {
   const CancelledTaskScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Cancelled"),
+    return ListView.separated(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return TaskCard();
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(height: 8,);
+      },
     );
   }
 }
