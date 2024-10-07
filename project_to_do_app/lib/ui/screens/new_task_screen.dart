b71d9b5c4a0889f2d9355b200e62ab1b539ae907
Rup Chowdhury <rupbd1@gmail.com>
2024-10-07 15:04:19 +0600

    converting card into a widget

--- project_to_do_app/lib/ui/screens/new_task_screen.dart
+++ project_to_do_app/lib/ui/screens/new_task_screen.dart
@@ -20,46 +20,7 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
           Expanded(child: ListView.separated(
               itemCount: 10,
               itemBuilder: (context, index) {
-            return Card(
-              elevation: 0,
-              color: Colors.white,
-              margin: const EdgeInsets.symmetric(horizontal: 16),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Title of the Task", style: Theme.of(context).textTheme.titleSmall,),
-                    const Text("Description of task",),
-                    const Text("Date: 20/10/2024",),
-                    const SizedBox(height: 8,),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Chip(
-                          label: const Text(
-                            "New", style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold
-                        ),), shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(24),
-                          side: const BorderSide(
-                            color: AppColor.themeColor
-                          )
-                        ),),
-                        Wrap(
-                          children: [
-                            IconButton(onPressed: (){}, icon: const Icon(Icons.edit)),
-                            IconButton(onPressed: (){}, icon: const Icon(Icons.delete)),
-                          ],
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            );
+            return TaskCard();
           },
           separatorBuilder: (context, index) {
                 return const SizedBox(height: 8,);
@@ -102,6 +63,56 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
 
   void _onTapAddFAB(){
     Navigator.push(context, MaterialPageRoute(builder: (context) => const AddNewTaskScreen()));
+  }
+}
+
+class TaskCard extends StatelessWidget {
+  const TaskCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      color: Colors.white,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Title of the Task", style: Theme.of(context).textTheme.titleSmall,),
+            const Text("Description of task",),
+            const Text("Date: 20/10/2024",),
+            const SizedBox(height: 8,),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Chip(
+                  label: const Text(
+                    "New", style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold
+                ),), shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24),
+                  side: const BorderSide(
+                    color: AppColor.themeColor
+                  )
+                ),),
+                Wrap(
+                  children: [
+                    IconButton(onPressed: (){}, icon: const Icon(Icons.edit)),
+                    IconButton(onPressed: (){}, icon: const Icon(Icons.delete)),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
