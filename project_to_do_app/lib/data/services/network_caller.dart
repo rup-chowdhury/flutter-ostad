98df74f8947988568409cc7a075ef5ac6a350c97
Rup Chowdhury <rupbd1@gmail.com>
2024-10-09 01:12:40 +0600

    Update network_caller.dart

--- project_to_do_app/lib/data/services/network_caller.dart
+++ project_to_do_app/lib/data/services/network_caller.dart
@@ -1,3 +1,20 @@
-class NetworkCaller{
-  
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:project_to_do_app/data/models/network_response.dart';
+
+class NetworkCaller {
+  Future<NetworkResponse> getRequest(String url) async {
+    Uri uri = Uri.parse(url);
+    try {
+      final Response response = await get(uri);
+      if (response.statusCode == 200) {
+        final decodeData = jsonDecode(response.body);
+        return NetworkResponse(
+            isSuccess: true,
+            statusCode: response.statusCode,
+            responseData: decodeData);
+      } else {}
+    } catch (e) {}
+  }
 }
