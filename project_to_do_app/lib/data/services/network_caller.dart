d4c40ada34f1209076e726f7645a45c2f47edee2
Rup Chowdhury <rupbd1@gmail.com>
2024-10-09 01:31:33 +0600

    Update network_caller.dart

--- project_to_do_app/lib/data/services/network_caller.dart
+++ project_to_do_app/lib/data/services/network_caller.dart
@@ -1,20 +1,34 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 import 'package:project_to_do_app/data/models/network_response.dart';
 
 class NetworkCaller {
   Future<NetworkResponse> getRequest(String url) async {
-    Uri uri = Uri.parse(url);
     try {
+      Uri uri = Uri.parse(url);
+      debugPrint(url);
       final Response response = await get(uri);
+      debugPrint('URL: $url\nRESPONSE CODE: ${response.statusCode}\nBODY: ${response.body}');
       if (response.statusCode == 200) {
         final decodeData = jsonDecode(response.body);
         return NetworkResponse(
             isSuccess: true,
             statusCode: response.statusCode,
             responseData: decodeData);
-      } else {}
-    } catch (e) {}
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+        );
+      }
+    } catch (e) {
+      return NetworkResponse(
+        isSuccess: false,
+        statusCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
   }
 }
