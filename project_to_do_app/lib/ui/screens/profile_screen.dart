cd7f3fbf5dcbf36e2249fa0f39e6c89b26133b14
Rup Chowdhury <rupbd1@gmail.com>
2024-10-08 18:29:29 +0600

    Update Profile form

--- project_to_do_app/lib/ui/screens/profile_screen.dart
+++ project_to_do_app/lib/ui/screens/profile_screen.dart
@@ -17,6 +17,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(height: 48,),
               Text(
@@ -24,10 +25,48 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 style: Theme.of(context).textTheme.displaySmall
                     ?.copyWith(fontWeight: FontWeight.w600),
               ),
+              const SizedBox(height: 16,),
+              _buildPhotoPicker(),
+              const SizedBox(height: 16,),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Email"
+                ),
+              ),
+              const SizedBox(height: 8,),
+              TextFormField(
+                decoration: const InputDecoration(
+                    hintText: "First Name"
+                ),
+              ),
+              const SizedBox(height: 8,),
+              TextFormField(
+                decoration: const InputDecoration(
+                    hintText: "Last Name"
+                ),
+              ),
+              const SizedBox(height: 8,),
+              TextFormField(
+                decoration: const InputDecoration(
+                    hintText: "Phone"
+                ),
+              ),
+              const SizedBox(height: 8,),
+              TextFormField(
+                decoration: const InputDecoration(
+                    hintText: "Password"
+                ),
+              ),
+              const SizedBox(height: 16,),
+              ElevatedButton(onPressed: (){}, child: const Icon(Icons.arrow_circle_right_outlined))
             ],
           ),
         ),
       ),
     );
+  }
+
+  Container _buildPhotoPicker(){
+    return Container();
   }
 }
