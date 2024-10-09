4fd63da0d0df932e9344d902591989c95c0ee943
Rup Chowdhury <rupbd1@gmail.com>
2024-10-09 16:30:14 +0600

    Update network_caller.dart

--- project_to_do_app/lib/data/services/network_caller.dart
+++ project_to_do_app/lib/data/services/network_caller.dart
@@ -1,16 +1,15 @@
 import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 import 'package:project_to_do_app/data/models/network_response.dart';
 
 class NetworkCaller {
-  Future<NetworkResponse> getRequest(String url) async {
+  static Future<NetworkResponse> getRequest(String url) async {
     try {
       Uri uri = Uri.parse(url);
       debugPrint(url);
       final Response response = await get(uri);
-      debugPrint('URL: $url\nRESPONSE CODE: ${response.statusCode}\nBODY: ${response.body}');
+      printResponse(url, response);
       if (response.statusCode == 200) {
         final decodeData = jsonDecode(response.body);
         return NetworkResponse(
@@ -30,5 +29,36 @@ class NetworkCaller {
         errorMessage: e.toString(),
       );
     }
+  }
+
+  static Future<NetworkResponse> postRequest(String url) async {
+    try {
+      Uri uri = Uri.parse(url);
+      debugPrint(url);
+      final Response response = await get(uri);
+      printResponse(url, response);
+      if (response.statusCode == 200) {
+        final decodeData = jsonDecode(response.body);
+        return NetworkResponse(
+            isSuccess: true,
+            statusCode: response.statusCode,
+            responseData: decodeData);
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+        );
+      }
+    } catch (e) {
+      return NetworkResponse(
+        isSuccess: false,
+        statusCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  static void printResponse(String url,Response response) {
+    debugPrint('URL: $url\nRESPONSE CODE: ${response.statusCode}\nBODY: ${response.body}');
   }
 }
