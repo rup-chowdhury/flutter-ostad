1ac9da7e7c379ad3059b6708a505786e7b85dbd9
Rup Chowdhury <rupbd1@gmail.com>
2024-10-18 22:47:29 +0600

    it will check if the user is logged in

--- project_to_do_app/lib/ui/screens/splash_screen.dart
+++ project_to_do_app/lib/ui/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:project_to_do_app/ui/controllers/auth_controller.dart';
 import 'package:project_to_do_app/ui/screens/sign_in_screen.dart';
 import 'package:project_to_do_app/ui/utils/assets_path.dart';
 import 'package:project_to_do_app/ui/widgets/screen_background.dart';
@@ -20,12 +21,15 @@ class _SplashScreenState extends State<SplashScreen> {
 
   Future<void> _moveToNextScreen() async {
     await Future.delayed(const Duration(seconds: 2));
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const SignInScreen(),
-      ),
-    );
+    await AuthController.getAccessToken();
+    if(AuthController.isLoggedIn()){
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const SignInScreen(),
+        ),
+      );
+    }
   }
 
   @override
