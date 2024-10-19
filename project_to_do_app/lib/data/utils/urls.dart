2b248c540cd8dff545754a9e4f20a6a7f91c60ad
Rup Chowdhury <rupbd1@gmail.com>
2024-10-19 22:28:32 +0600

    add URL into the variable

--- project_to_do_app/lib/data/utils/urls.dart
+++ project_to_do_app/lib/data/utils/urls.dart
@@ -3,5 +3,6 @@ class Urls {
 
   static const String registrationUrl = '$_baseUrl/Registration';
   static const String loginUrl = '$_baseUrl/Login';
+  static const String addNewTaskUrl = '$_baseUrl/createTask';
 
 }
