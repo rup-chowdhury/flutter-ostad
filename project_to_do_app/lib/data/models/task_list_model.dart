994dd2622885da3ddba1f2d9668a1c1db9eb6946
Rup Chowdhury <rupbd1@gmail.com>
2024-10-23 22:48:56 +0600

    taskList model

---
+++ project_to_do_app/lib/data/models/task_list_model.dart
@@ -0,0 +1,62 @@
+class TaskListModel {
+  String? status;
+  List<Data>? data;
+
+  TaskListModel({this.status, this.data});
+
+  TaskListModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    if (json['data'] != null) {
+      data = <Data>[];
+      json['data'].forEach((v) {
+        data!.add(new Data.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = this.status;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Data {
+  String? sId;
+  String? title;
+  String? description;
+  String? status;
+  String? email;
+  String? createdDate;
+
+  Data(
+      {this.sId,
+        this.title,
+        this.description,
+        this.status,
+        this.email,
+        this.createdDate});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    title = json['title'];
+    description = json['description'];
+    status = json['status'];
+    email = json['email'];
+    createdDate = json['createdDate'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['title'] = this.title;
+    data['description'] = this.description;
+    data['status'] = this.status;
+    data['email'] = this.email;
+    data['createdDate'] = this.createdDate;
+    return data;
+  }
+}
