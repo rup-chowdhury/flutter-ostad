320353247ab0760e92298f202e2cde7cec69d144
Rup Chowdhury <rupbd1@gmail.com>
2024-10-23 23:47:00 +0600

    Update task_list_model.dart

--- project_to_do_app/lib/data/models/task_list_model.dart
+++ project_to_do_app/lib/data/models/task_list_model.dart
@@ -1,3 +1,5 @@
+import 'package:project_to_do_app/data/models/task_model.dart';
+
 class TaskListModel {
   String? status;
   List<TaskModel>? taskList;
@@ -15,28 +17,4 @@ class TaskListModel {
   }
 }
 
-class TaskModel {
-  String? sId;
-  String? title;
-  String? description;
-  String? status;
-  String? email;
-  String? createdDate;
 
-  TaskModel(
-      {this.sId,
-        this.title,
-        this.description,
-        this.status,
-        this.email,
-        this.createdDate});
-
-  TaskModel.fromJson(Map<String, dynamic> json) {
-    sId = json['_id'];
-    title = json['title'];
-    description = json['description'];
-    status = json['status'];
-    email = json['email'];
-    createdDate = json['createdDate'];
-  }
-}
