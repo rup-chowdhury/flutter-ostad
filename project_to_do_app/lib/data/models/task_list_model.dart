9d5195df4f02896268fa0b35e2051342ee83475b
Rup Chowdhury <rupbd1@gmail.com>
2024-10-23 22:49:51 +0600

    Update task_list_model.dart

--- project_to_do_app/lib/data/models/task_list_model.dart
+++ project_to_do_app/lib/data/models/task_list_model.dart
@@ -13,15 +13,6 @@ class TaskListModel {
       });
     }
   }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['status'] = this.status;
-    if (this.data != null) {
-      data['data'] = this.data!.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
 }
 
 class Data {
