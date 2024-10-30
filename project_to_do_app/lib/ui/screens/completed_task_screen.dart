787d7dfc59f6bbb0b4b4f64a865e519fc9b3c8de
Rup Chowdhury <rupbd1@gmail.com>
2024-10-30 13:41:57 +0600

    converting to stateful

--- project_to_do_app/lib/ui/screens/completed_task_screen.dart
+++ project_to_do_app/lib/ui/screens/completed_task_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:project_to_do_app/ui/widgets/task_card.dart';
 
-class CompletedTaskScreen extends StatelessWidget {
+class CompletedTaskScreen extends StatefulWidget {
   const CompletedTaskScreen({super.key});
 
+  @override
+  State<CompletedTaskScreen> createState() => _CompletedTaskScreenState();
+}
+
+class _CompletedTaskScreenState extends State<CompletedTaskScreen> {
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
