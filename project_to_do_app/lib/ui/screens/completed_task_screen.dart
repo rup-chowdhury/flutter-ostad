2a203dda4b48beb31aafcadbe1d8063497a2002a
Rup Chowdhury <rupbd1@gmail.com>
2024-10-30 14:17:08 +0600

    completed task screen done

--- project_to_do_app/lib/ui/screens/completed_task_screen.dart
+++ project_to_do_app/lib/ui/screens/completed_task_screen.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:project_to_do_app/data/models/network_response.dart';
+import 'package:project_to_do_app/data/models/task_list_model.dart';
+import 'package:project_to_do_app/data/models/task_model.dart';
+import 'package:project_to_do_app/data/services/network_caller.dart';
+import 'package:project_to_do_app/ui/widgets/centered_circular_progress_indicator.dart';
+import 'package:project_to_do_app/ui/widgets/snack_bar_message.dart';
 import 'package:project_to_do_app/ui/widgets/task_card.dart';
+
+import '../../data/utils/urls.dart';
 
 class CompletedTaskScreen extends StatefulWidget {
   const CompletedTaskScreen({super.key});
@@ -9,16 +17,52 @@ class CompletedTaskScreen extends StatefulWidget {
 }
 
 class _CompletedTaskScreenState extends State<CompletedTaskScreen> {
+  bool _getCompleteTaskListInProgress = false;
+  List<TaskModel> _completedTaskList =  [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getCompleteTaskList();
+  }
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        // return const TaskCard();
-      },
-      separatorBuilder: (context, index) {
-        return const SizedBox(height: 8,);
-      },
+    return Visibility(
+      visible: !_getCompleteTaskListInProgress,
+      replacement: const CenteredCircularProgressIndicator(),
+      child: RefreshIndicator(
+        onRefresh: () async {
+          _getCompleteTaskList();
+        },
+        child: ListView.separated(
+          itemCount: _completedTaskList.length,
+          itemBuilder: (context, index) {
+            return TaskCard(
+              taskModel: _completedTaskList[index],
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: 8,);
+          },
+        ),
+      ),
     );
+  }
+
+  Future<void> _getCompleteTaskList() async {
+    _completedTaskList.clear();
+    _getCompleteTaskListInProgress = true;
+    setState(() {});
+    final NetworkResponse response =
+    await NetworkCaller.getRequest(url: Urls.completedTaskList);
+    if(response.isSuccess) {
+      final TaskListModel taskListModel = TaskListModel.fromJson(response.responseData);
+      _completedTaskList = taskListModel.taskList ?? [];
+    } else {
+      showSnackBarMessage(context, response.errorMessage, true);
+      print(response.errorMessage);
+    }
+    _getCompleteTaskListInProgress = false;
+    setState(() {});
   }
 }
