21c673a67344ee674d8b28e4807b1d09b26a7f3c
Rup Chowdhury <rupbd1@gmail.com>
2024-10-30 10:41:11 +0600

    New task URL added

--- project_to_do_app/lib/data/utils/urls.dart
+++ project_to_do_app/lib/data/utils/urls.dart
@@ -1,8 +1,8 @@
 class Urls {
-  static const String _baseUrl = 'http://152.42.163.176:2006/api/v1';
+  static const String _baseUrl = 'http://35.73.30.144:2005/api/v1';
 
   static const String registrationUrl = '$_baseUrl/Registration';
   static const String loginUrl = '$_baseUrl/Login';
   static const String addNewTaskUrl = '$_baseUrl/createTask';
-
+  static const String newTaskList = '$_baseUrl/listTaskByStatus/New';
 }
