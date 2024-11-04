2b4c6ddfbff76202a48e0563c9b5fe2fb4e2411d
Rup Chowdhury <rupbd1@gmail.com>
2024-11-04 23:07:50 +0600

    progress screen done

--- project_to_do_app/lib/ui/screens/progress_task_screen.dart
+++ project_to_do_app/lib/ui/screens/progress_task_screen.dart
@@ -1,19 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:project_to_do_app/data/models/network_response.dart';
+import 'package:project_to_do_app/data/models/task_list_model.dart';
+import 'package:project_to_do_app/data/models/task_model.dart';
+import 'package:project_to_do_app/data/services/network_caller.dart';
+import 'package:project_to_do_app/data/utils/urls.dart';
 import 'package:project_to_do_app/ui/widgets/task_card.dart';
 
-class ProgressTaskScreen extends StatelessWidget {
+import '../widgets/snack_bar_message.dart';
+
+class ProgressTaskScreen extends StatefulWidget {
   const ProgressTaskScreen({super.key});
 
   @override
+  State<ProgressTaskScreen> createState() => _ProgressTaskScreenState();
+}
+
+class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
+  bool _getProgressTaskListInProgress = false;
+  List<TaskModel> _progressTaskList =  [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getProgressTaskList();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        // return TaskCard();
-      },
-      separatorBuilder: (context, index) {
-        return const SizedBox(height: 8,);
-      },
+    return Visibility(
+      visible: !_getProgressTaskListInProgress,
+      child: RefreshIndicator(
+        onRefresh: () async {
+          _getProgressTaskList();
+        },
+        child: ListView.separated(
+          itemCount: _progressTaskList.length,
+          itemBuilder: (context, index) {
+            return TaskCard(
+              taskModel: _progressTaskList[index],
+              onRefreshList: _getProgressTaskList);
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: 8,);
+          },
+        ),
+      ),
     );
+  }
+
+  Future<void> _getProgressTaskList() async {
+    _progressTaskList.clear();
+    _getProgressTaskListInProgress = true;
+    setState(() {});
+    final NetworkResponse response =
+    await NetworkCaller.getRequest(url: Urls.progressTaskList);
+    if(response.isSuccess) {
+      final TaskListModel taskListModel = TaskListModel.fromJson(response.responseData);
+      _progressTaskList = taskListModel.taskList ?? [];
+    } else {
+      showSnackBarMessage(context, response.errorMessage, true);
+      print(response.errorMessage);
+    }
+    _getProgressTaskListInProgress = false;
+    setState(() {});
   }
 }
