6f38d8a8addc69bb339b9cb3a4a295281e3ad001
Rup Chowdhury <rupbd1@gmail.com>
2024-11-08 17:33:41 +0600

    set controllers in the textfields

--- project_to_do_app/lib/ui/screens/profile_screen.dart
+++ project_to_do_app/lib/ui/screens/profile_screen.dart
@@ -50,30 +50,35 @@ class _ProfileScreenState extends State<ProfileScreen> {
               _buildPhotoPicker(),
               const SizedBox(height: 16,),
               TextFormField(
+                controller: _emailTEController,
                 decoration: const InputDecoration(
                   hintText: "Email"
                 ),
               ),
               const SizedBox(height: 8,),
               TextFormField(
+                controller: _firstNameTEController,
                 decoration: const InputDecoration(
                     hintText: "First Name"
                 ),
               ),
               const SizedBox(height: 8,),
               TextFormField(
+                controller: _lastNameTEController,
                 decoration: const InputDecoration(
                     hintText: "Last Name"
                 ),
               ),
               const SizedBox(height: 8,),
               TextFormField(
+                controller: _phoneTEController,
                 decoration: const InputDecoration(
                     hintText: "Phone"
                 ),
               ),
               const SizedBox(height: 8,),
               TextFormField(
+                controller: _passwordTEController,
                 decoration: const InputDecoration(
                     hintText: "Password"
                 ),
