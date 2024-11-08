0689a6c01efadf581fbc10ffd2db45c1ff26d99b
Rup Chowdhury <rupbd1@gmail.com>
2024-11-08 17:41:53 +0600

    Update profile_screen.dart

--- project_to_do_app/lib/ui/screens/profile_screen.dart
+++ project_to_do_app/lib/ui/screens/profile_screen.dart
@@ -16,6 +16,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   final TextEditingController _phoneTEController = TextEditingController();
   final TextEditingController _passwordTEController = TextEditingController();
   // final TextEditingController _emailTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -37,56 +38,63 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 48,),
-              Text(
-                "Get Started With",
-                style: Theme.of(context).textTheme.displaySmall
-                    ?.copyWith(fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(height: 32,),
-              _buildPhotoPicker(),
-              const SizedBox(height: 16,),
-              TextFormField(
-                controller: _emailTEController,
-                enabled: false,
-                decoration: const InputDecoration(
-                  hintText: "Email"
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 48,),
+                Text(
+                  "Get Started With",
+                  style: Theme.of(context).textTheme.displaySmall
+                      ?.copyWith(fontWeight: FontWeight.w600),
                 ),
-              ),
-              const SizedBox(height: 8,),
-              TextFormField(
-                controller: _firstNameTEController,
-                decoration: const InputDecoration(
-                    hintText: "First Name"
+                const SizedBox(height: 32,),
+                _buildPhotoPicker(),
+                const SizedBox(height: 16,),
+                TextFormField(
+                  controller: _emailTEController,
+                  enabled: false,
+                  decoration: const InputDecoration(
+                    hintText: "Email"
+                  ),
                 ),
-              ),
-              const SizedBox(height: 8,),
-              TextFormField(
-                controller: _lastNameTEController,
-                decoration: const InputDecoration(
-                    hintText: "Last Name"
+                const SizedBox(height: 8,),
+                TextFormField(
+                  controller: _firstNameTEController,
+                  decoration: const InputDecoration(
+                      hintText: "First Name"
+                  ),
                 ),
-              ),
-              const SizedBox(height: 8,),
-              TextFormField(
-                controller: _phoneTEController,
-                decoration: const InputDecoration(
-                    hintText: "Phone"
+                const SizedBox(height: 8,),
+                TextFormField(
+                  controller: _lastNameTEController,
+                  decoration: const InputDecoration(
+                      hintText: "Last Name"
+                  ),
                 ),
-              ),
-              const SizedBox(height: 8,),
-              TextFormField(
-                controller: _passwordTEController,
-                decoration: const InputDecoration(
-                    hintText: "Password"
+                const SizedBox(height: 8,),
+                TextFormField(
+                  controller: _phoneTEController,
+                  decoration: const InputDecoration(
+                      hintText: "Phone"
+                  ),
                 ),
-              ),
-              const SizedBox(height: 16,),
-              ElevatedButton(onPressed: (){}, child: const Icon(Icons.arrow_circle_right_outlined))
-            ],
+                const SizedBox(height: 8,),
+                TextFormField(
+                  controller: _passwordTEController,
+                  decoration: const InputDecoration(
+                      hintText: "Password"
+                  ),
+                ),
+                const SizedBox(height: 16,),
+                ElevatedButton(onPressed: (){
+                  if(_formKey.currentState!.validate()) {
+
+                  }
+                }, child: const Icon(Icons.arrow_circle_right_outlined))
+              ],
+            ),
           ),
         ),
       ),
