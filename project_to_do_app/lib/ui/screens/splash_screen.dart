d7e77f982dcb1eb7101f841849a6e19d5e962b29
Rup Chowdhury <rupbd1@gmail.com>
2024-11-11 23:11:32 +0600

    fixing username and email not showing issue

--- project_to_do_app/lib/ui/screens/splash_screen.dart
+++ project_to_do_app/lib/ui/screens/splash_screen.dart
@@ -24,6 +24,7 @@ class _SplashScreenState extends State<SplashScreen> {
     await Future.delayed(const Duration(seconds: 2));
     await AuthController.getAccessToken();
     if(AuthController.isLoggedIn()){
+      await AuthController.getUserData();
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
