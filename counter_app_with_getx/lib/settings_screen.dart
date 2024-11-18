8bf053f08b8015a50df4e31acdc2f6c5d2da35bd
Rup Chowdhury <rupbd1@gmail.com>
2024-11-18 23:09:32 +0600

    Update settings_screen.dart

--- counter_app_with_getx/lib/settings_screen.dart
+++ counter_app_with_getx/lib/settings_screen.dart
@@ -5,6 +5,15 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings Screen'),
+      ),
+      body: Center(
+        child: Column(
+          
+        ),
+      ),
+    );
   }
 }
