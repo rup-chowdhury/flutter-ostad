bf9874ef9d2fd33954a72937e2fb3d7ebded6153
Rup Chowdhury <rupbd1@gmail.com>
2024-11-19 17:40:49 +0600

    Create profile_screen.dart

---
+++ counter_app_with_getx/lib/profile_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(onPressed: (){
+              // Navigator.pop(context);
+              Get.back();
+            }, child:
+            const Text('Back'))
+          ],
+        ),
+      ),
+    );
+  }
+}
