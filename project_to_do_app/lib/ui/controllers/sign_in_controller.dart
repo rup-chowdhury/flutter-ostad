7a21b6108a52a517de94995e1ee456970835fc6e
Rup Chowdhury <rupbd1@gmail.com>
2024-11-25 14:03:44 +0600

    controller for sign in screen

--- project_to_do_app/lib/ui/controllers/sign_in_controller.dart
+++ project_to_do_app/lib/ui/controllers/sign_in_controller.dart
@@ -1,5 +1,44 @@
 import 'package:get/get.dart';
+import 'package:project_to_do_app/data/models/login_model.dart';
+import 'package:project_to_do_app/data/models/network_response.dart';
+import 'package:project_to_do_app/data/services/network_caller.dart';
+import 'package:project_to_do_app/data/utils/urls.dart';
+import 'package:project_to_do_app/ui/controllers/auth_controller.dart';
 
 class SignInController extends GetxController{
-  
+  bool _inProgress = false;
+
+  bool get inProgress => _inProgress;
+
+  String? _errorMessage;
+
+  String? get errorMessage => _errorMessage;
+
+  Future<bool> signIn(String email, String password) async{
+    bool isSuccess = false;
+    _inProgress = true;
+    update();
+
+    Map<String, dynamic> requestBody = {
+      "email": email,
+      "password": password
+    };
+
+    final NetworkResponse response = await NetworkCaller.postRequest(url: Urls.loginUrl, body: requestBody);
+
+
+    if(response.isSuccess) {
+      LoginModel loginModel = LoginModel.fromJson(response.responseData);
+      await AuthController.saveAccessToken(loginModel.token!);
+      await AuthController.saveUserData(loginModel.data!);
+      _inProgress = true;
+    } else {
+      _errorMessage = response.errorMessage;
+    }
+
+    _inProgress = false;
+    update();
+
+    return isSuccess;
+  }
 }
