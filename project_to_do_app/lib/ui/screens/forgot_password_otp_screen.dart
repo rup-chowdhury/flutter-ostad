372ea8875b8884822d9422444101ee33a1d5a357
Rup Chowdhury <rupbd1@gmail.com>
2024-11-26 23:13:59 +0600

    Update forgot_password_otp_screen.dart

--- project_to_do_app/lib/ui/screens/forgot_password_otp_screen.dart
+++ project_to_do_app/lib/ui/screens/forgot_password_otp_screen.dart
@@ -149,7 +149,7 @@ class _ForgotPasswordOTPScreenState extends State<ForgotPasswordOTPScreen> {
       }
     }
 
-  void _onTapForgotPassword()
+  void 
 
   void _onTapSignIn() {
     Navigator.pushAndRemoveUntil(
