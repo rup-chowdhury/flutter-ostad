2363bfd93867a093b756b35b12ea88b00f3ad435
Rup Chowdhury <rupbd1@gmail.com>
2024-11-29 22:56:00 +0600

    Update new_task_list_controller.dart

--- project_to_do_app/lib/ui/controllers/new_task_list_controller.dart
+++ project_to_do_app/lib/ui/controllers/new_task_list_controller.dart
@@ -1,5 +1,28 @@
 import 'package:get/get.dart';
 
 class NewTaskListController extends GetxController{
-  
+  bool _inProgress = false;
+
+  String? _errorMessage;
+
+  bool get inProgress => _inProgress;
+
+  String? get errorMessage => _errorMessage;
+
+  Future<bool> getNewTaskList() async {
+    bool isSuccess = false;
+    _inProgress = true;
+    setState(() {});
+    final NetworkResponse response =
+    await NetworkCaller.getRequest(url: Urls.newTaskList);
+    if(response.isSuccess) {
+      final TaskListModel taskListModel = TaskListModel.fromJson(response.responseData);
+      _newTaskList = taskListModel.taskList ?? [];
+    } else {
+      showSnackBarMessage(context, response.errorMessage, true);
+      print(response.errorMessage);
+    }
+    _getNewTaskListInProgress = false;
+    setState(() {});
+  }
 }
