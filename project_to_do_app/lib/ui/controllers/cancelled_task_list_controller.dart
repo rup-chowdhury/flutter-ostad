638044c6e8f0a357f3312cb1a6096aa1f17e7e79
Rup Chowdhury <rupbd1@gmail.com>
2024-11-30 22:16:02 +0600

    cancelled_task_list_controller done

--- project_to_do_app/lib/ui/controllers/cancelled_task_list_controller.dart
+++ project_to_do_app/lib/ui/controllers/cancelled_task_list_controller.dart
@@ -1,5 +1,9 @@
 import 'package:get/get.dart';
+import 'package:project_to_do_app/data/models/network_response.dart';
+import 'package:project_to_do_app/data/models/task_list_model.dart';
 import 'package:project_to_do_app/data/models/task_model.dart';
+import 'package:project_to_do_app/data/services/network_caller.dart';
+import 'package:project_to_do_app/data/utils/urls.dart';
 
 class CancelledTaskListController extends GetxController{
   bool _inProgress = false;
@@ -12,7 +16,24 @@ class CancelledTaskListController extends GetxController{
 
   String? get errorMessage => _errorMessage;
 
-  List<TaskModel> get TaskList => _taskList;
+  List<TaskModel> get taskList => _taskList;
 
-  
+  Future<bool> getCancelledTaskList() async {
+    bool isSuccess = false;
+    _inProgress = true;
+    update();
+    final NetworkResponse response =
+    await NetworkCaller.getRequest(url: Urls.canceledTaskList);
+    if(response.isSuccess) {
+      final TaskListModel taskListModel = TaskListModel.fromJson(response.responseData);
+      _taskList = taskListModel.taskList ?? [];
+      isSuccess = true;
+    } else {
+      _errorMessage = response.errorMessage;
+    }
+    _inProgress = false;
+    update();
+
+    return isSuccess;
+  }
 }
