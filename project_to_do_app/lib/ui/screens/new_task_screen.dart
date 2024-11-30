68b786244f134254ae6c653b94eff6ee7d7d62ce
Rup Chowdhury <rupbd1@gmail.com>
2024-11-30 16:38:29 +0600

    Update new_task_screen.dart

--- project_to_do_app/lib/ui/screens/new_task_screen.dart
+++ project_to_do_app/lib/ui/screens/new_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:project_to_do_app/data/models/network_response.dart';
 import 'package:project_to_do_app/data/models/task_list_model.dart';
 import 'package:project_to_do_app/data/models/task_model.dart';
@@ -6,6 +7,7 @@ import 'package:project_to_do_app/data/models/task_status_count_model.dart';
 import 'package:project_to_do_app/data/models/task_status_model.dart';
 import 'package:project_to_do_app/data/services/network_caller.dart';
 import 'package:project_to_do_app/data/utils/urls.dart';
+import 'package:project_to_do_app/ui/controllers/new_task_list_controller.dart';
 import 'package:project_to_do_app/ui/screens/add_new_task_screen.dart';
 import 'package:project_to_do_app/ui/widgets/centered_circular_progress_indicator.dart';
 import 'package:project_to_do_app/ui/widgets/snack_bar_message.dart';
@@ -22,12 +24,11 @@ class NewTaskScreen extends StatefulWidget {
 }
 
 class _NewTaskScreenState extends State<NewTaskScreen> {
-  bool _getNewTaskListInProgress = false;
   bool _getTaskStatusCountListInProgress = false;
-
-  List<TaskModel> _newTaskList = [];
   List<TaskStatusModel> _taskStatusCountList = [];
 
+  final NewTaskListController _newTaskListController =
+      Get.find<NewTaskListController>();
 
   @override
   void initState() {
@@ -35,6 +36,7 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
     _getNewTaskList();
     _getTaskStatusCount();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -92,22 +94,23 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
     );
   }
 
-  List<TaskSummaryCard> _getTaskSummaryCardList(){
+  List<TaskSummaryCard> _getTaskSummaryCardList() {
     List<TaskSummaryCard> taskSummaryCardList = [];
-    for(TaskStatusModel t in _taskStatusCountList ){
-      taskSummaryCardList.add(TaskSummaryCard(count: t.sum ?? 0, title: t.sId!));
+    for (TaskStatusModel t in _taskStatusCountList) {
+      taskSummaryCardList
+          .add(TaskSummaryCard(count: t.sum ?? 0, title: t.sId!));
     }
 
     return taskSummaryCardList;
   }
 
   Future<void> _onTapAddFAB() async {
-    final bool? shouldRefresh = await Navigator.push(context,
+    final bool? shouldRefresh = await Navigator.push(
+        context,
         MaterialPageRoute(
-            builder: (context) => const AddNewTaskScreen(),
-        )
-    );
-    if(shouldRefresh == true){
+          builder: (context) => const AddNewTaskScreen(),
+        ));
+    if (shouldRefresh == true) {
       _getNewTaskList();
     }
     // if()
@@ -119,8 +122,9 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
     setState(() {});
     final NetworkResponse response =
         await NetworkCaller.getRequest(url: Urls.newTaskList);
-    if(response.isSuccess) {
-      final TaskListModel taskListModel = TaskListModel.fromJson(response.responseData);
+    if (response.isSuccess) {
+      final TaskListModel taskListModel =
+          TaskListModel.fromJson(response.responseData);
       _newTaskList = taskListModel.taskList ?? [];
     } else {
       showSnackBarMessage(context, response.errorMessage, true);
@@ -135,10 +139,10 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
     _getTaskStatusCountListInProgress = true;
     setState(() {});
     final NetworkResponse response =
-    await NetworkCaller.getRequest(url: Urls.taskStatusCount);
-    if(response.isSuccess) {
+        await NetworkCaller.getRequest(url: Urls.taskStatusCount);
+    if (response.isSuccess) {
       final TaskStatusCountModel taskStatusCountModel =
-      TaskStatusCountModel.fromJson(response.responseData);
+          TaskStatusCountModel.fromJson(response.responseData);
       _taskStatusCountList = taskStatusCountModel.taskStatusCountList ?? [];
     } else {
       showSnackBarMessage(context, response.errorMessage, true);
