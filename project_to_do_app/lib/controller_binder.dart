07555d826fc7f21e49f1633e6950f9a1edd38fff
Rup Chowdhury <rupbd1@gmail.com>
2024-11-30 15:45:48 +0600

    add NewTaskListController into controller binder dependency

--- project_to_do_app/lib/controller_binder.dart
+++ project_to_do_app/lib/controller_binder.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
+import 'package:project_to_do_app/ui/controllers/new_task_list_controller.dart';
 import 'package:project_to_do_app/ui/controllers/sign_in_controller.dart';
 
 class ControllerBinder extends Bindings{
   @override
   void dependencies() {
     Get.put(SignInController());
+    Get.put(NewTaskListController());
   }
 
 }
