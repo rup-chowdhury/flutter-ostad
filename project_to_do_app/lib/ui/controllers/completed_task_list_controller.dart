77ab1063333ad7ae331f429fd439d879af17cc92
Rup Chowdhury <rupbd1@gmail.com>
2024-12-02 11:20:13 +0600

    declaring variables and getters

--- project_to_do_app/lib/ui/controllers/completed_task_list_controller.dart
+++ project_to_do_app/lib/ui/controllers/completed_task_list_controller.dart
@@ -2,5 +2,15 @@ import 'package:get/get.dart';
 import 'package:project_to_do_app/data/models/task_model.dart';
 
 class CompletedTaskListController extends GetxController{
-  
- }
+  bool _inProgress = false;
+
+  String? _errorMessage;
+
+  List<TaskModel> _taskList = [];
+
+  bool get inProgress => _inProgress;
+
+  String? get errorMessage => _errorMessage;
+
+  List<TaskModel> get taskList => _taskList;
+}
