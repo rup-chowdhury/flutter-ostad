d067353aeb40587be36a3a1ba120d1fe834a659f
Rup Chowdhury <rupbd1@gmail.com>
2024-12-05 14:20:10 +0600

    Update main.dart

--- firebase_practice/lib/main.dart
+++ firebase_practice/lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_practice/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
