cb580f019fa8474a0c343b4cd8abad6d8b08949c
Rup Chowdhury <rupbd1@gmail.com>
2024-12-05 14:15:06 +0600

    add firebase to main function

--- firebase_practice/lib/main.dart
+++ firebase_practice/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_practice/firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
