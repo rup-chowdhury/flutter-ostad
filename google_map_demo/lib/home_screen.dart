0fea755f97fe359a6a6f20e9a70c1490e2747b2c
Rup Chowdhury <rupbd1@gmail.com>
2024-12-09 15:12:44 +0600

    using markers property putting 2 markers on the 2 locations

--- google_map_demo/lib/home_screen.dart
+++ google_map_demo/lib/home_screen.dart
@@ -36,6 +36,22 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           zoom: 32,
         ),
+        markers: <Marker>{
+          const Marker(
+            markerId: MarkerId('initial-position'),
+            position: LatLng(
+              23.83762441058588,   //LongLat of MIST Tower 3
+              90.35722629592206,
+            )
+          ),
+          const Marker(
+              markerId: MarkerId('pollobi-metro'),
+              position: LatLng(
+                23.82610317065891,
+                90.36407638895095,
+              )
+          ),
+        },
       ),
     );
   }
