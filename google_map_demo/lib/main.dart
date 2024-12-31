832f7fb07d8798ffd174b682a72bee8022d40862
Rup Chowdhury <rupbd1@gmail.com>
2024-12-31 11:30:55 +0600

    setting sign up screen as the first screen

--- google_map_demo/lib/main.dart
+++ google_map_demo/lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:google_map_demo/home_screen.dart';
+import 'package:google_map_demo/screens/sign_up_screen.dart';
 
 void main() {
-  runApp(const GoogleMapsApp());
+  runApp(MyApp());
 }
 
-class GoogleMapsApp extends StatelessWidget {
-  const GoogleMapsApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: true,
-      home: HomeScreen(),
-    );
-  }
-}
+// class GoogleMapsApp extends StatelessWidget {
+//   const GoogleMapsApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: true,
+//       home: HomeScreen(),
+//     );
+//   }
+// }
