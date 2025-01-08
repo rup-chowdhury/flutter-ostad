a09c60e6183444b491832ed529e5f602717998d9
Rup Chowdhury <rupbd1@gmail.com>
2025-01-08 12:21:22 +0600

    .

---
+++ google_map_demo_try_fixing_one/android/app/src/main/java/com/example/google_map_demo_try_fixing_one/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.google_map_demo_try_fixing_one;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- google_map_demo_try_fixing_one/android/app/src/main/kotlin/com/example/google_map_demo_try_fixing_one/MainActivity.kt
+++ google_map_demo_try_fixing_one/android/app/src/main/kotlin/com/example/google_map_demo_try_fixing_one/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.google_map_demo
+package com.example.google_map_demo_try_fixing_one
 
 import io.flutter.embedding.android.FlutterActivity
 
