25a3198ab0c29cf7046772a7c873106502166655
Rup Chowdhury <rupbd1@gmail.com>
2025-02-04 14:30:41 +0600

    creating a class for transforming Lat Lng to Address

---
+++ google_map_demo/lib/widgets/TransformLatLngToAddress/transform_lat_lng.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TransformLatLng extends StatefulWidget {
+
+  @override
+  State<TransformLatLng> createState() => _State();
+}
+
+class _State extends State<TransformLatLng> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
