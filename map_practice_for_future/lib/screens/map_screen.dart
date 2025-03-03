214c8e459a17d04233d6477e62768af7d17e6711
Rup Chowdhury <rupbd1@gmail.com>
2025-03-03 14:46:11 +0600

    loading map with GoogleMap() widget

--- map_practice_for_future/lib/screens/map_screen.dart
+++ map_practice_for_future/lib/screens/map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -8,8 +9,13 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  static const LatLng _initialPosition = LatLng(23.838405415619437, 90.3595992615412);
+
   @override
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GoogleMap(initialCameraPosition: CameraPosition(target: _initialPosition)),
+    );
   }
 }
