cd5593f8c8d7699a1d55d64b84cc1dc1e63f060f
Rup Chowdhury <rupbd1@gmail.com>
2025-03-09 09:52:49 +0600

    import flutter polylines package

--- map_practice_for_future/lib/screens/map_screen.dart
+++ map_practice_for_future/lib/screens/map_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
