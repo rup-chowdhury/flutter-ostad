6b4690596ed006a3fd687fb999e6e4047d651253
Rup Chowdhury <rupbd1@gmail.com>
2025-03-12 10:06:00 +0600

    constants done

--- map_practice_for_future/lib/helpers/constants.dart
+++ map_practice_for_future/lib/helpers/constants.dart
@@ -1,8 +1,13 @@
 
 
-const GOOGLE_MAPS_API_KEY = "AIzaSyC_MPgcB-GAIUYap_caF_lQdB1UqFIEhMg";
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_google_places_hoc081098/google_maps_webservice_places.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+const GOOGLE_MAPS_API_KEY = "AIzaSyA3KP1kyVmShHUoei0xZhy0J6RNUiHiEBg";
 const COUNTRY = "country";
-Firestore firebaseFiretore = Firestore.instance;
+FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
 FirebaseAuth auth = FirebaseAuth.instance;
-FirebaseMessaging fcm = FirebaseMessaging();
+FirebaseMessaging fcm = FirebaseMessaging.instance;
 GoogleMapsPlaces places = GoogleMapsPlaces(apiKey: GOOGLE_MAPS_API_KEY);
