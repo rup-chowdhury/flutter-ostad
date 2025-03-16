111258426b6200415da837aef557ff6c48bf8993
Rup Chowdhury <rupbd1@gmail.com>
2025-03-16 12:29:23 +0600

    map app safe

--- map_practice_for_future/lib/main.dart
+++ map_practice_for_future/lib/main.dart
@@ -3,11 +3,11 @@ import 'package:map_practice_for_future/screens/home_screen.dart';
 import 'package:map_practice_for_future/screens/map_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
--- map_practice_for_future/lib/screens/map_screen.dart
+++ map_practice_for_future/lib/screens/map_screen.dart
@@ -4,11 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:map_practice_for_future/screens/search_places_screen.dart';
 import 'package:map_practice_for_future/values.dart';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({super.key});
-
   @override
   State<MapScreen> createState() => _MapScreenState();
 }
@@ -24,6 +23,9 @@ class _MapScreenState extends State<MapScreen> {
       Completer<GoogleMapController>();
 
   LatLng? _currentPosition = null;
+
+  late TextEditingController destinationTextEditingController =
+      TextEditingController();
 
   Map<PolylineId, Polyline> polylines = {};
 
@@ -42,56 +44,99 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _currentPosition == null
-          ? const Center(
-              child: Text(
-                'Loading.....',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18),
-              ),
-            )
-          : GoogleMap(
-              myLocationEnabled: true,
-              myLocationButtonEnabled: true,
-              trafficEnabled: true,
-              mapType: MapType.hybrid,
-              buildingsEnabled: false,
-              compassEnabled: true,
-              mapToolbarEnabled: true,
-              onLongPress: (LatLng destiny) {
-                setDestination(destiny);
-              },
-              onTap: (argument) => drawPolyline(),
-              onMapCreated: ((GoogleMapController controller) =>
-                  _mapController.complete(controller)),
-              initialCameraPosition: CameraPosition(
-                target: _initialPosition,
-                zoom: 13,
-              ),
-              markers: {
-                Marker(
-                  markerId: MarkerId("_currentLocation"),
-                  icon: BitmapDescriptor.defaultMarkerWithHue(184),
-                  position: _currentPosition!,
+      body: Stack(children: [
+        _currentPosition == null
+            ? const Center(
+                child: Text(
+                  'Loading.....',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
                 ),
-                // Marker(
-                //   markerId: MarkerId("_sourceLocation"),
-                //   icon: BitmapDescriptor.defaultMarker,
-                //   position: _initialPosition,
-                //   draggable: true
-                // ),
-                Marker(
-                  markerId: MarkerId("_destinationLocation"),
-                  icon: BitmapDescriptor.defaultMarker,
-                  position: _dhakaAirportPosition,
-                  draggable: true,
-                  onDragEnd: setDestination,
+              )
+            : buildGoogleMap(),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: destinationTextEditingController,
+                  onTap: () {
+                    // _addRoute(_startLocation, _endLocation);
+                  },
+                  decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white70,
+                    focusColor: Colors.white,
+                    prefixIcon: Icon(Icons.search),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                          width: 2,
+                        )),
+                    border: OutlineInputBorder(),
+                    hintText: 'Destination',
+                  ),
                 ),
-              },
-              polylines: Set<Polyline>.of(polylines.values),
+              ],
             ),
+          ),
+        ),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // DestinationSelectionWidget();
+        },
+        child: const Icon(Icons.home),
+      ),
+    );
+  }
+
+  GoogleMap buildGoogleMap() {
+    return GoogleMap(
+      myLocationEnabled: true,
+      myLocationButtonEnabled: true,
+      trafficEnabled: true,
+      mapType: MapType.hybrid,
+      buildingsEnabled: false,
+      compassEnabled: true,
+      mapToolbarEnabled: true,
+      onLongPress: (LatLng destiny) {
+        setDestination(destiny);
+      },
+      onTap: (argument) => drawPolyline(),
+      onMapCreated: ((GoogleMapController controller) =>
+          _mapController.complete(controller)),
+      initialCameraPosition: CameraPosition(
+        target: _initialPosition,
+        zoom: 13,
+      ),
+      markers: {
+        Marker(
+          markerId: MarkerId("_currentLocation"),
+          icon: BitmapDescriptor.defaultMarkerWithHue(184),
+          position: _currentPosition!,
+        ),
+        // Marker(
+        //   markerId: MarkerId("_sourceLocation"),
+        //   icon: BitmapDescriptor.defaultMarker,
+        //   position: _initialPosition,
+        //   draggable: true
+        // ),
+        Marker(
+          markerId: MarkerId("_destinationLocation"),
+          icon: BitmapDescriptor.defaultMarker,
+          position: _dhakaAirportPosition,
+          draggable: true,
+          onDragEnd: setDestination,
+        ),
+      },
+      polylines: Set<Polyline>.of(polylines.values),
     );
   }
 
@@ -142,14 +187,20 @@ class _MapScreenState extends State<MapScreen> {
   Future<List<LatLng>> getPolylinePoints() async {
     List<LatLng> polyLineCoordinates = [];
     PolylinePoints polylinePoints = PolylinePoints();
+    // PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+    // googleApiKey: GOOGLE_MAPS_API_KEY,
+    // request: PolylineRequest(
+    //     origin: PointLatLng(
+    //         _currentPosition!.latitude, _currentPosition!.longitude),
+    //     destination: PointLatLng(_dhakaAirportPosition.latitude,
+    //         _dhakaAirportPosition.longitude),
+    //     mode: TravelMode.driving)
+    //    );
     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-        googleApiKey: GOOGLE_MAPS_API_KEY,
-        request: PolylineRequest(
-            origin: PointLatLng(
-                _currentPosition!.latitude, _currentPosition!.longitude),
-            destination: PointLatLng(_dhakaAirportPosition.latitude,
-                _dhakaAirportPosition.longitude),
-            mode: TravelMode.driving));
+        GOOGLE_MAPS_API_KEY,
+        PointLatLng(_currentPosition!.latitude, _currentPosition!.longitude),
+        PointLatLng(
+            _dhakaAirportPosition.latitude, _dhakaAirportPosition.longitude));
     if (result.points.isNotEmpty) {
       result.points.forEach((PointLatLng point) {
         polyLineCoordinates.add(LatLng(point.latitude, point.longitude));
