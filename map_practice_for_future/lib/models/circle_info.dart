a2c211b0f56b9d0968a153cd770710d8f89cb681
Rup Chowdhury <rupbd1@gmail.com>
2025-04-27 09:40:06 +0600

    variables in the model

--- map_practice_for_future/lib/models/circle_info.dart
+++ map_practice_for_future/lib/models/circle_info.dart
@@ -1,3 +1,20 @@
-class CircleInfo{
+class CircleInfo {
+  final String id;
+  final String fillColor;
+  final double fillOpacity;
+  final double centerLat;
+  final double centerLng;
+  final double radius;
+  final String strokeColor;
+  final int strokeWidth;
 
+  CircleInfo(
+      {required this.id,
+      required this.fillColor,
+      required this.fillOpacity,
+      required this.centerLat,
+      required this.centerLng,
+      required this.radius,
+      required this.strokeColor,
+      required this.strokeWidth});
 }
