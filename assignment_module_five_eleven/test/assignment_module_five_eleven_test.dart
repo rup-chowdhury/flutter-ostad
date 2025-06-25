8cda70f9631515572e8bbacc69fc3f135fceb588
Rup Chowdhury <rupbd1@gmail.com>
2025-06-25 23:57:06 +0600

    assingment module 5

---
+++ assignment_module_five_eleven/test/assignment_module_five_eleven_test.dart
@@ -0,0 +1,8 @@
+import 'package:assignment_module_five_eleven/assignment_module_five_eleven.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
