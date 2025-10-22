51742261f6113ba41b24d71ab7768d1d0a07a4b9
Rup Chowdhury <rupbd1@gmail.com>
2025-10-22 16:43:24 +0200

    .

--- project_to_do_app/lib/ui/utils/assets_path.dart
+++ project_to_do_app/lib/ui/utils/assets_path.dart
@@ -4,4 +4,5 @@ class AssetsPath {
   static const String logoSvg = "$_imagePath/logo.svg";
   static const String backgroundSvg = "$_imagePath/background.svg";
   static const String profilePicJpg = "$_imagePath/rup_image.jpg";
+
 }
