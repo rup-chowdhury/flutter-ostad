ee277b4f9bc1f31f263b0b8b4bb1526e43b049eb
Rup Chowdhury <rupbd1@gmail.com>
2025-10-22 16:22:02 +0200

    Update network_response.dart

--- project_to_do_app/lib/data/models/network_response.dart
+++ project_to_do_app/lib/data/models/network_response.dart
@@ -8,5 +8,6 @@ class NetworkResponse {
       {required this.isSuccess,
       required this.statusCode,
       this.responseData,
-      this.errorMessage = 'Something went wrong'});
+      this.errorMessage = 'Something went wrong'}
+      );
 }
